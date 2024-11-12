4c1c59a31a065076d840aecfbc65af4abbe359de
prash48 <111002751+prash48@users.noreply.github.com>
2024-11-12 07:14:44 +0000

    swapping of two numbers in javascript.

---
+++ swap.js
@@ -0,0 +1,10 @@
+// swapping of two numbers without using third variable with arithmetic operator...
+const prompt = require("prompt-sync")({sigint: true}) //for Code run in VScode you have to write this code.
+let a = prompt("Enter the value of a:")
+let b = prompt("Enter the value of b:")
+a = Number.parseInt(a)
+b = number.parseInt(b)
+a = a + b;
+b = a - b;
+a = a - b;
+console.log("After swapping the value os a and b is:" + a + " , " +b)
