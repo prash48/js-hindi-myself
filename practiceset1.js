29a6566c452cfaf98ebadfe93633053c57b74117
prash48 <111002751+prash48@users.noreply.github.com>
2024-11-14 16:02:05 +0000

    Practice set 1

---
+++ practiceset1.js
@@ -0,0 +1,39 @@
+//Prob 1.Create a type of string and try to add a number to it.
+let a = "prashant"
+let b = 6
+console.log(a + b)
+
+
+//Prob 2.use typeof operator to find the data type of the string in last question.
+let c = "prashant"
+let d = 46
+console.log(c + d)
+console.log(typeof (c + d))
+
+
+//Prob 3.ceate a const object in javascript can you change it to hold a number later.
+const b1 = {name: "prashantkumar",
+    section: 2,
+    isprincipal: true
+}
+a = "prashantkumar"
+
+
+//Prob 4.Try to add a new key to the const object in problem 3 were you able to do it.
+const a1 = {
+    name: "Harry",
+    section: 1,
+    isprincipal: false
+}
+a1['friend'] = "Naina"
+a1['name'] = "Bittu"
+console.log(a1)
+
+
+//Prob 5.Write a js program to create a word meaning dictionary of 5 words.
+const dict = {
+    appericate: "reconige the full work of",
+    ataraxia: "a state of freedom from emotional disturbance and anexity",
+    yakka: "work, especially Hard work."
+}
+console.log(dict.yakka)
